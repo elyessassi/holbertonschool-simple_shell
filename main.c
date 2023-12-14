a4e4dee4cf509662c36ee3d60bb5453705a94bee
elyessassi <mjoxen@gmail.com>
2023-12-14 14:39:33 +0100

    main program

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 #include "header.h"
+#include <errno.h>
 
 /**
- * 
-*/
+ *
+ */
 
 int main(int argc, char *argv[])
-{  
+
+{
     char *n = NULL;
 
-    argc = argc;
-    argv = argv;
-    
-
     while (1)
-    {   
+    {
         n = readline();
-        
+        (void)argc;
+
         if (n == NULL)
         {
             printf("\n");
-            return(0);
+            return (0);
         }
-        printf("%s", n);
+        execv(argv[0], argv);
+        printf("%d\n", errno);
     }
     return (1);
 }
