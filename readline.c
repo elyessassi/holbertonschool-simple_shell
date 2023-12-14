9b14a195f6d0f3c36337d8c570a2fba78c5efc9d
elyessassi <mjoxen@gmail.com>
2023-12-14 10:40:01 +0100

    readline

---
+++ readline.c
@@ -0,0 +1,24 @@
+#include "header.h"
+
+
+/**
+ * 
+*/
+
+char *readline(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n ;
+    if (isatty(STDIN_FILENO))
+    {
+        write(STDOUT_FILENO, "$ ", 2);
+        n = getline(&line, &len, stdin);
+	    if (n == -1)
+	    {
+		    free(line);
+		    return (NULL);
+	    }
+    }
+    return (line);
+}
