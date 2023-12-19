042c3d0aeba4daed522761bb1e6ced9e598bdabe
elyessassi <mjoxen@gmail.com>
2023-12-19 10:14:06 +0100

    readline

--- readline.c
+++ readline.c
@@ -1,24 +1,21 @@
 #include "header.h"
 
-
 /**
- * 
-*/
+ *
+ */
 
 char *readline(void)
 {
     char *line = NULL;
     size_t len = 0;
-    ssize_t n ;
+    ssize_t n;
     if (isatty(STDIN_FILENO))
-    {
         write(STDOUT_FILENO, "($) ", 4);
-        n = getline(&line, &len, stdin);
-	    if (n == -1)
-	    {
-		    free(line);
-		    return (NULL);
-	    }
+    n = getline(&line, &len, stdin);
+    if (n == -1)
+    {
+        free(line);
+        return (NULL);
     }
     return (line);
 }
