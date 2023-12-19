54edcefcf999e40003adcab11e58cf7fc5e735b3
Elyesrafa <elyesarfa7211@gmail.com>
2023-12-19 14:13:42 +0100

    updated header file

---
+++ gentenv.c
@@ -0,0 +1,11 @@
+#include "header.h"
+/*
+ *
+*/
+char *getenv(char *name)
+{
+    int i;
+    for (i = 0; env[i]; i++)
+    prit
+
+}
--- header.h
+++ header.h
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+
 
 extern char **environ;
 
