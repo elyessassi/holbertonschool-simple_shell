000bc3defc438fad1bf46ce7f156aa15cf0e4b2d
Elyesrafa <elyesarfa7211@gmail.com>
2023-12-20 20:38:25 +0100

    updated exucute file

--- execute.c
+++ execute.c
@@ -1,31 +1,42 @@
 #include "header.h"
 
 /**
- * 
-*/
-
-int exec(char **argv, char **array )
+ * Execute a command using fork and execve
+ */
+int exec(char **argv, char **array)
 {
     pid_t forkval;
     int status;
+    int exit_code;
 
     forkval = fork();
     if (forkval == 0)
     {
-        if(execve(array[0], array, NULL) == -1)
+       
+        if (execve(array[0], array, NULL) == -1)
         {
             perror(argv[0]);
             freearray(array);
-            exit(0);
+            exit(EXIT_FAILURE);
         }
-    }
-    else
+    } 
+    else if (forkval > 0)
     {
         waitpid(forkval, &status, 0);
+        if (WIFEXITED(status)) {
+            exit_code = WEXITSTATUS(status);
+        }
+        else
+        {
+        exit_code = 1;  
+        }
         freearray(array);
+        return exit_code;
     }
-    return (0);
-    
-
-
+    else
+    {  
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
 }
