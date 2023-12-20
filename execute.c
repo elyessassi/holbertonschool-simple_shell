0a12a428b497f572cf709293d89328b5a967cd76
Elyesrafa <elyesarfa7211@gmail.com>
2023-12-20 21:21:01 +0100

    test

--- execute.c
+++ execute.c
@@ -7,7 +7,7 @@ int exec(char **argv, char **array)
 {
     pid_t forkval;
     int status;
-    int exit_code;
+    int exit_status;
     forkval = fork();
     if (forkval == 0)
     {
@@ -17,19 +17,19 @@ int exec(char **argv, char **array)
             freearray(array);
             exit(EXIT_FAILURE);
         }
-    }
-    else if (forkval > 0)
+    } else if (forkval > 0)
     {
         waitpid(forkval, &status, 0);
-        if (WIFEXITED(status)) {
-            exit_code = WEXITSTATUS(status);
+        if (WIFEXITED(status))
+        {
+            exit_status = WEXITSTATUS(status);
         }
         else
         {
-            exit_code = 1;
+            exit_status = 1;
         }
         freearray(array);
-        return exit_code;
+        return exit_status;
     }
     else
     {
