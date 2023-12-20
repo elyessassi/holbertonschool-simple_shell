f7120aa55a8b8b5d022ed9f28c85ee1d035fe5be
Elyesrafa <elyesarfa7211@gmail.com>
2023-12-20 20:19:03 +0100

    updated builtin.c file

--- builtin.c
+++ builtin.c
@@ -4,16 +4,13 @@
 */
 int builtin(char *cmd)
 {
-    char *builtin[] = {
-        "exit, env", NULL
-    };
+    char *builtin[] = { "exit", "env", NULL };
     int i;
-    for (i = 0; builtin[i]; i++)
-    {
+    for (i = 0; builtin[i]; i++) {
         if (strcmp(cmd, builtin[i]) == 0)
-        return (1);
+            return 1;
     }
-    return(0);
+    return 0;
 }
 
 /*
@@ -22,9 +19,13 @@ int builtin(char *cmd)
 void handlebuiltin(char **cmd, int status)
 {
     if (strcmp(cmd[0], "exit") == 0)
-    exit_shell(cmd, status);
+        exit_shell(cmd, status);
     else if (strcmp(cmd[0], "env") == 0)
-    print_env(cmd, status);
+        print_env(cmd, status);
+    else {
+        fprintf(stderr, "Unknown command: %s\n", cmd[0]);
+        freearray(cmd);
+    }
 }
 
 /*
@@ -39,14 +40,13 @@ void exit_shell(char **cmd, int status)
 /*
 *
 */
-void print_env(char ** cmd, int status)
- {
+void print_env(char **cmd, int status)
+{
     int i;
-    (void) status;
-    for (i = 0; environ[i]; i++)
-    {
+    (void)status;
+    for (i = 0; environ[i]; i++) {
         write(STDOUT_FILENO, environ[i], strlen(environ[i]));
         write(STDOUT_FILENO, "\n", 1);
     }
     freearray(cmd);
- }
+}
