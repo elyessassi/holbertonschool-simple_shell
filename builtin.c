34e5c43d43ab300e29f135f4952b77e3853a71f8
Elyesarfa <elyesarfa@gmail.com>
2023-12-21 15:13:01 +0100

    testing

--- builtin.c
+++ builtin.c
@@ -6,11 +6,14 @@ int builtin(char *cmd)
 {
     char *builtin[] = { "exit", "env", NULL };
     int i;
-    for (i = 0; builtin[i]; i++) {
+    for (i = 0; builtin[i]; i++)
+    {
         if (strcmp(cmd, builtin[i]) == 0)
-            return 1;
+        {
+            return (1);
+        }
     }
-    return 0;
+    return (0);
 }
 
 /*
