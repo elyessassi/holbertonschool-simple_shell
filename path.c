6d080a6513fb2a496016a76764b5320fc814c7f7
elyessassi <mjoxen@gmail.com>
2023-12-21 11:46:52 +0100

    get path

---
+++ path.c
@@ -0,0 +1,37 @@
+#include "header.h"
+
+/**
+ * 
+*/
+
+char *path(char *command)
+{
+    char *env;
+    char *test;
+    char *full;
+    struct stat state;
+
+    if (stat(command, &state) == 0)
+        return(command)
+    else
+    {
+        test = strtok(env, ":");
+        while (test)
+	    {
+		    full = malloc(strlen(test) + strlen(command) + 2);
+		    if (full)
+		    {
+			    strcpy(full, test);
+			    strcat(full, "/");
+			    strcat(full, command);
+			    if (stat(full, &state) == 0)
+			    {
+				    free(full);
+				    return (full);
+			    }
+			    full = my_strtok(NULL, ":");
+		    }
+        
+        }
+    }
+}
