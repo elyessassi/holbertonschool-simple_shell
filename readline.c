03a5cc1ae0dd3b5347a5e39af8ca00bad95bb809
elyessassi <mjoxen@gmail.com>
2023-12-22 04:00:01 +0100

    betty

--- readline.c
+++ readline.c
@@ -1,23 +1,24 @@
 #include "header.h"
 /**
- * Reads a line of input from the user.
+ * readline - Reads a line of input from the user.
  * If in interactive mode, displays a prompt "$ ".
  *
- * @return A dynamically allocated string containing the input line.
+ * Return: A dynamically allocated string containing the input line.
  *         Returns NULL on failure or end of input.
  */
 char *readline(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t n;
-    if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "$ ", 2);
-    n = getline(&line, &len, stdin);
-    if (n == -1)
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+	n = getline(&line, &len, stdin);
+	if (n == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
