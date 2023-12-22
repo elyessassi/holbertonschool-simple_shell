0598bdf8369f91fac6f9429c0d2fb4f5fd9e5899
elyessassi <mjoxen@gmail.com>
2023-12-22 03:49:46 +0100

    betty

--- builtin.c
+++ builtin.c
@@ -1,20 +1,20 @@
 #include "header.h"
 /**
- * Checks if the given command is a built-in command.
- *
- * @param cmd The command to be checked.
- * @return 1 if the command is built-in, 0 otherwise.
+ * builtin - Checks if the given command is a built-in command.
+ * @cmd: The command to be checked.
+ * Return: 1 if the command is built-in, 0 otherwise.
  */
 int builtin(char *cmd)
 {
-    char *builtin[] = { "exit", "env", NULL };
-    int i;
-    for (i = 0; builtin[i]; i++)
-    {
-        if (strcmp(cmd, builtin[i]) == 0)
-        {
-            return (1);
-        }
-    }
-    return (0);
+	char *builtin[] = {"exit", "env", NULL};
+	int i;
+
+	for (i = 0; builtin[i]; i++)
+	{
+		if (strcmp(cmd, builtin[i]) == 0)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
