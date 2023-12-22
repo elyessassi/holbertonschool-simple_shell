02b2e39fdcc06dc2da5de1e4ef1f7094ed70e0c6
elyessassi <mjoxen@gmail.com>
2023-12-22 03:58:55 +0100

    betty

--- mygetenv.c
+++ mygetenv.c
@@ -1,29 +1,29 @@
 #include "header.h"
 /**
- * Retrieves the value of the environment variable "PATH."
+ * mygetenv - Retrieves the value of the environment variable "PATH."
  *
- * @return A string containing the value of "PATH" or NULL if not found.
+ * Return: A string containing the value of "PATH" or NULL if not found.
  */
 char *mygetenv(void)
 {
-    char *nametest;
-    char *name = "PATH";
-    char *temp;
+	char *nametest;
+	char *name = "PATH";
+	char *temp;
 
-    temp = strdup(*environ);
-    nametest = strtok(temp, "=");
-    while ((nametest != NULL) || (nametest != name))
-    {
-        if (strcmp(name, nametest) == 0)
-        {
-            nametest = strtok(NULL, "\n");
-            return(nametest);
-        }
-        else
-        {
-            temp = strdup(*++environ);
-            nametest = strtok(temp, "=");
-        }
-    }
-    return (0);
+	temp = strdup(*environ);
+	nametest = strtok(temp, "=");
+	while ((nametest != NULL) || (nametest != name))
+	{
+		if (strcmp(name, nametest) == 0)
+		{
+			nametest = strtok(NULL, "\n");
+			return (nametest);
+		}
+		else
+		{
+			temp = strdup(*++environ);
+			nametest = strtok(temp, "=");
+		}
+	}
+	return (0);
 }
