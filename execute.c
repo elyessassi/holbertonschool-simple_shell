ba34664e7235a01b22265190e808a2532b6a4213
elyessassi <mjoxen@gmail.com>
2023-12-22 03:52:49 +0100

    betty

--- execute.c
+++ execute.c
@@ -1,41 +1,44 @@
 #include "header.h"
 /**
- * Executes a command based on the provided arguments and environment.
- *
- * @param argv The command-line arguments.
- * @param array The array representing the command.
- * @return The exit status of the executed command.
+ * exec - Executes a command based on the provided arguments and environment.
+ * @argv: The command-line arguments.
+ * @array: array The array representing the command.
+ * Return: The exit status of the executed command.
  */
 int exec(char **argv, char **array)
 {
-    pid_t forkval;
-    int status;
-    int exit_status;
+	pid_t forkval;
+	int status;
+	int exit_status;
 
-    forkval = fork();
-    if (forkval == 0)
-    {
-        if (execve(array[0], array, NULL) == -1)
-        {
-            perror(argv[0]);
-            freearray(array);
-            exit(EXIT_FAILURE);
-        }
-    } else if (forkval > 0) {
-        waitpid(forkval, &status, 0);
-        if (WIFEXITED(status))
-        {
-            exit_status = WEXITSTATUS(status);
-        }
-        else
-        {
-            exit_status = 1;
-        }
-        freearray(array);
-        return exit_status;
-    } else {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    return 0;
+	forkval = fork();
+	if (forkval == 0)
+	{
+		if (execve(array[0], array, NULL) == -1)
+		{
+			perror(argv[0]);
+			freearray(array);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (forkval > 0)
+	{
+		waitpid(forkval, &status, 0);
+		if (WIFEXITED(status))
+		{
+			exit_status = WEXITSTATUS(status);
+		}
+		else
+		{
+			exit_status = 1;
+		}
+		freearray(array);
+		return (exit_status);
+	}
+	else
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
 }
