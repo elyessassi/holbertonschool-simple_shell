98256ae89125d7fd086ddf85ff384147e5c27afc
elyessassi <mjoxen@gmail.com>
2023-12-22 03:54:14 +0100

    betty

--- freearray.c
+++ freearray.c
@@ -1,16 +1,15 @@
 #include "header.h"
 /**
- * Frees the memory allocated for a dynamically allocated string array.
- *
- * @param array The array to be freed.
+ * freearray - Frees the memory allocated for a 2d array.
+ * @array: The array to be freed.
  */
 void freearray(char **array)
 {
-    int i;
+	int i;
 
-    for (i = 0 ; array[i] != NULL ; i++)
-    {
-        free(array[i]);
-    }
-    free(array);
+	for (i = 0 ; array[i] != NULL ; i++)
+	{
+		free(array[i]);
+	}
+	free(array);
 }
