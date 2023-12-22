822ec97e6b3f63a3a38efba1c0c1238262e5e321
elyessassi <mjoxen@gmail.com>
2023-12-22 04:04:33 +0100

    split

--- spliter.c
+++ spliter.c
@@ -2,54 +2,51 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * Splits a string into an array of strings based on delimiters.
+ * split - Splits a string into an array of strings based on delimiters.
  *
- * @param input The string to be split.
- * @return An array of strings.
+ * @line: The string to be split.
+ * Return: An array of strings.
  */
 char **split(char *line)
 {
-    char *temp = NULL;
-    int x = 0;
-    int i = 0;
-    char *d = " \t\n";
-    char* token = NULL;
-    char **array = NULL;
+	char *temp = NULL, token = NULL,  d = " \t\n";
+	int x = 0, i = 0;
+	char **array = NULL;
 
-    if (!line)
-    {
-        free(line);
-        return(NULL);
-    }
-    temp = strdup(line);
-    token = strtok(temp, d);
-    if (token == NULL)
-    {
-        free(line);
-        free(temp);
-        return(NULL);
-    }
-    while (token)
-    {
-        x++;
-        token = strtok(NULL, d);
-    }
-    free(temp);
-    array = (char**)malloc(sizeof(char *) * (x + 1));
-    if (array == NULL)
-    {
-        free(line);
-        return (NULL);
-    }
-    token = strtok(line, d);
-    while (token != NULL)
-    {
-        array[i] = strdup(token);
-        i++;
-        token = strtok(NULL, d);
-    }
-    array[i] = NULL;
-    free(line);
-    free(token);
-    return(array);
+	if (!line)
+	{
+		free(line);
+		return (NULL);
+	}
+	temp = strdup(line);
+	token = strtok(temp, d);
+	if (token == NULL)
+	{
+		free(line);
+		free(temp);
+		return (NULL);
+	}
+	while (token)
+	{
+		x++;
+		token = strtok(NULL, d);
+	}
+	free(temp);
+	array = (char **)malloc(sizeof(char *) * (x + 1));
+	if (array == NULL)
+	{
+		free(line);
+		return (NULL);
+	}
+	token = strtok(line, d);
+	while (token != NULL)
+	{
+		array[i] = strdup(token);
+		i++;
+		token = strtok(NULL, d);
+	}
+	array[i] = NULL;
+	free(line);
+	free(token);
+	return (array);
 }
